4602c604074d68f80eaef73ef82be425fa950ab2
ravibundheliya <ravibundheliya880@gmail.com>
2023-12-03 20:17:42 +0530

    new slider

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function activephoto1() {
+    var i1 = document.getElementById('img_top_1');
+    var i2 = document.getElementById('img_top_2');
+    var i3 = document.getElementById('img_top_3');
+    i1.style.display = "block";
+    i2.style.display = "none";
+    i3.style.display = "none";
+}
